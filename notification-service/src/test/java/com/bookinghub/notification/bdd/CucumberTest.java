90f532d27fa2c035485b58ed4e404b547b005a6f
Gustavo Leite <gustavo.jog@hotmail.com>
2026-04-11 17:37:32 -0300

    fix: cucumber bdd

--- notification-service/src/test/java/com/bookinghub/notification/bdd/CucumberTest.java
+++ notification-service/src/test/java/com/bookinghub/notification/bdd/CucumberTest.java
@@ -11,5 +11,5 @@ import org.junit.platform.suite.api.Suite;
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.bookinghub.notification.bdd")
-public class CucumberRunner {
+public class CucumberTest {
 }
